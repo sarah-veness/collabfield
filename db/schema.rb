9bec72b14fbfd7adb0644a29f9ef4c5fcbb2af96
Sarah Veness <sarahhveness@gmail.com>
2023-07-19 15:58:15 -0700

    - add name column to db - include name attribute to sign_up_params * account_update_params

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_223710) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
