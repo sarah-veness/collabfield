352c00242688a5811657c2f1a014a02e47ff574d
Sarah Veness <sarahhveness@gmail.com>
2023-07-24 17:43:31 -0700

    add factorybot and selenium config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,5 @@
-  require 'capybara/rspec'
+require 'capybara/rspec'
+require 'selenium-webdriver'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
@@ -40,28 +41,31 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
 
+  
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
- 
+  
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
- 
+  
   config.before(:each, :js => true) do
     DatabaseCleaner.strategy = :truncation
   end
- 
+  
   config.before(:each) do
     DatabaseCleaner.start
   end
- 
+  
   config.after(:each) do
     DatabaseCleaner.clean
   end
-
+  
+  config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :feature
   Capybara.server = :puma 
+
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
