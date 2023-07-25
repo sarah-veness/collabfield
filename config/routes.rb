ab047594cb86a8a872a89467cd5e52052e518da4
Sarah Veness <sarahhveness@gmail.com>
2023-07-24 22:51:41 -0700

    add logout test

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+
   end
 end
